b9641643a6a7980555dcbb891cc37e6cfa629e31
Marina Huber <huber.marina@gmail.com>
2022-06-23 17:51:23 +0100

    initial commit

---
+++ LandscapeAppApproval/LandscapeAppApprovalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandscapeAppApprovalApp.swift
+//  LandscapeAppApproval
+//
+//  Created by Marina Huber on 23.06.2022..
+//
+
+import SwiftUI
+
+@main
+struct LandscapeAppApprovalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
